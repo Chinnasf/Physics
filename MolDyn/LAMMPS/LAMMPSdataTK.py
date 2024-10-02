91620ebf2973eeffae00886ee70e70454f5d8e50
Chinnasf <chinnasf@outlook.de>
2024-10-02 13:05:57 +0200

    tk

--- MolDyn/LAMMPS/LAMMPSdataTK.py
+++ MolDyn/LAMMPS/LAMMPSdataTK.py
@@ -5,20 +5,19 @@ import seaborn as sns
 import re
 import os
 
+"""
+RECEIVE WRITE DATA FROM LAMMPS TO BE PROCESSED FOR WHATEVER USE
+"""
 
-
-def get_file(filename):
-    """
-    returns raw dataframe from any type of file
-    """
-    with open(filename, "r") as file:
+def get_file(filename_):
+    with open(filename_, "r") as file:
         file_content = file.readlines()
     df = pd.DataFrame(file_content, columns=['data'])
     return df
 
 
-def get_structure(ionomer,slices):
-    data = ionomer.loc[slices[0]:slices[1]].copy().reset_index(drop=True)
+def get_structure(filename_,slices):
+    data = get_file(filename_)[slices[0]:slices[1]].copy().reset_index(drop=True)
     data["data"] = data["data"].str.split('\s').apply(lambda x: list(filter(None, x)))
     data["atom id"] = data["data"].apply(lambda x: x[0]).astype(int)
     data["molecule id"] = data["data"].apply(lambda x: x[1]).astype(int)
@@ -27,13 +26,10 @@ def get_structure(ionomer,slices):
     data["x"] = data["data"].apply(lambda x: x[4]).astype(float)
     data["y"] = data["data"].apply(lambda x: x[5]).astype(float)
     data["z"] = data["data"].apply(lambda x: x[6]).astype(float)
-    # REMARKS: Based on DOI:10.1063/1.4894813 
-    data["elements"] = data["atom type"].map({1:"CB",2:"C",3:"F",4:"O",5:"S",6:"H"})
-    data = data.sort_values("atom id").reset_index(drop=True)
     return data
 
-def get_bonds(ionomer,slices):
-    data = ionomer[slices[0]:slices[1]].copy().reset_index(drop=True)
+def get_bonds(filename_,slices):
+    data = get_file(filename_)[slices[0]:slices[1]].copy().reset_index(drop=True)
     data["data"] = data["data"].str.split('\s').apply(lambda x: list(filter(None, x)))
     data["bond id"] = data["data"].apply(lambda x: x[0]).astype(int)
     data["bond type"] = data["data"].apply(lambda x: x[1]).astype(int)
@@ -42,8 +38,8 @@ def get_bonds(ionomer,slices):
     return data
 
 
-def get_angles(data, slices):
-    angles = data[slices[0]:slices[1]].copy().reset_index(drop=True)
+def get_angles(filename_, slices):
+    angles = get_file(filename_)[slices[0]:slices[1]].copy().reset_index(drop=True)
     angles["data"] = angles["data"].str.split('\s').apply(lambda x: list(filter(None, x)))
     angles["angle id"] = angles["data"].apply(lambda x: x[0]).astype(int)
     angles["angle type"] = angles["data"].apply(lambda x: x[1]).astype(int)
@@ -53,8 +49,8 @@ def get_angles(data, slices):
     return angles
 
 
-def get_dihedrals(data,slices):
-    dihedrals = data[slices[0]:slices[1]].copy().reset_index(drop=True)
+def get_dihedrals(filename_,slices):
+    dihedrals = get_file(filename_)[slices[0]:slices[1]].copy().reset_index(drop=True)
     dihedrals["data"] = dihedrals["data"].str.split('\s').apply(lambda x: list(filter(None, x)))
     dihedrals["dihedral id"] = dihedrals["data"].apply(lambda x: x[0]).astype(int)
     dihedrals["dihedral type"] = dihedrals["data"].apply(lambda x: x[1]).astype(int)
@@ -63,3 +59,15 @@ def get_dihedrals(data,slices):
     dihedrals["atom3"] = dihedrals["data"].apply(lambda x: x[4]).astype(int)
     dihedrals["atom4"] = dihedrals["data"].apply(lambda x: x[5]).astype(int)
     return dihedrals
+
+
+def get_impropers(filename_,slices):
+    impropers = get_file(filename_)[slices[0]:slices[1]].copy().reset_index(drop=True)
+    impropers["data"] = impropers["data"].str.split('\s').apply(lambda x: list(filter(None, x)))
+    impropers["improper id"] = impropers["data"].apply(lambda x: x[0]).astype(int)
+    impropers["improper type"] = impropers["data"].apply(lambda x: x[1]).astype(int)
+    impropers["atom1"] = impropers["data"].apply(lambda x: x[2]).astype(int)
+    impropers["atom2"] = impropers["data"].apply(lambda x: x[3]).astype(int)
+    impropers["atom3"] = impropers["data"].apply(lambda x: x[4]).astype(int)
+    impropers["atom4"] = impropers["data"].apply(lambda x: x[5]).astype(int)
+    return impropers
